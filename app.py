890383ac26fb69e069fad996ce609a2460430bc5
dschloe <j2hoon85@gmail.com>
2023-04-24 11:35:29 +0900

    File Updated

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # -*- coding:UTF-8 -*-
 import streamlit as st
 import pandas as pd
+from utils import p_lans
+from PIL import Image
 
 def main():
     # title
@@ -48,6 +50,57 @@ def main():
     st.title('write()')
     st.write(iris)
 
+    myCode = """
+    def hello():
+        print("hi")
+    """
+    st.code(myCode, language="Python")
+
+    # 위젯, button 기능활용
+    name = 'Evan'
+    if st.button('Submit'):
+        st.write(f'name: {name.upper()}')
+
+    # RadioButton
+    s_state = st.radio('Status', ('활성화', '비활성화'))
+    if s_state == '활성화':
+        st.success('활성화 상태')
+    else:
+        st.error('비활성화 상태')
+
+    # Check Box
+    if st.checkbox('show/hide'):
+        st.text('무언가를 보여줘!!')
+
+    # Select Box
+    choice = st.selectbox('프로그래밍 언어', p_lans)
+    st.write(f'{choice} 언어를 선택함')
+
+    # multiple selection
+    lans = ("영어", "일본어", "중국어", "독일어")
+    myChoice = st.multiselect("언어 선택", lans, default="중국어")
+    st.write("선택", myChoice)
+
+    # Slider
+    age = st.slider('나이', 1, 120)
+    st.write(age)
+
+    # 이미지 가져오기
+    img = Image.open('data/image_03.jpg')
+    st.image(img)
+
+    url = 'data/image_03.jpg'
+    st.image(url)
+
+    # 비디오 출력
+    with open('data/secret_of_success.mp4', 'rb') as rb:
+        video_file = rb.read()
+        st.video(video_file, start_time=1)
+
+    # 오디오 출력
+    with open('data/song.mp3', 'rb') as rb:
+        audio_file = rb.read()
+        st.audio(audio_file, format="audio/mp3")
 
 
 if __name__ == "__main__":
