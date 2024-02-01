e7de82e715fb972d4637f8071f6c76136a2195f5
dschloe <j2hoon85@gmail.com>
2024-02-01 17:57:02 +0900

    updated

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
-# -*- coding:utf-8 -*-
-import streamlit as st
-from utils import html_temp
-from utils import dec_temp
-from eda_app import run_eda_app
-from ml_app import run_ml_app
-from stat_app import run_stat_app
+import streamlit as st 
+import pandas as pd 
+import requests 
+import json 
+import pandas as pd 
+
+@st.cache_data
+def load_data():
+    SEOUL_PUBLIC_API = st.secrets["api_credentials"]["SEOUL_PUBLIC_API"]
+    st.write(SEOUL_PUBLIC_API)
+    URL = f'http://openapi.seoul.go.kr:8088/{SEOUL_PUBLIC_API}/json/tbLnOpendataRtmsV/1/100/'
+    st.write(URL)
+    content = requests.get(URL).json()
+    data = pd.DataFrame(content['tbLnOpendataRtmsV']['row'])
+    return data
 
 def main():
-    st.markdown(html_temp, unsafe_allow_html=True)
+    result = load_data()
+    st.data_editor(result)
+    SGG_NM = st.selectbox("지역구를 선택하세요", sorted(list(result['SGG_NM'].unique())))
+    st.write(SGG_NM)
 
-    menu = ['HOME', 'EDA', 'ML', 'STAT', 'About']
-    choice = st.sidebar.selectbox('Menu', menu)
-
-    if choice == 'HOME':
-        st.subheader('HOME')
-        st.markdown(dec_temp, unsafe_allow_html=True)
-    elif choice == 'EDA':
-        run_eda_app()
-    elif choice == 'ML':
-        run_ml_app()
-    elif choice == 'STAT':
-        run_stat_app()
-    else:
-        st.subheader('About')
+    st.data_editor(result.loc[result['SGG_NM'] == SGG_NM, :])
 
 if __name__ == "__main__":
     main()
